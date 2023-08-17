3187ca368236c3e568b9583edd2e465806635a95
rahulbabu7 <rahulbabu436@gmail.com>
2023-08-17 22:52:47 +0530

    added basic files

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        <h1>
+            About
+        </h1>
+    </header>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+ <header>
+    <h1>
+        Home 
+    </h1>
+ </header>
+</template>
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
